24149929b139bf63d54ccb2b2db773abb84c527d
Michael Weiser <m.weiser@science-computing.de>
2013-03-23 19:13:23 +0100

    Add NS_MAXMSG constant
    - Add NS_MAXMSG constant for systems where arpa/nameser.h doesn't define it (atleast glibc < 2.5).

--- msktname.cpp
+++ msktname.cpp
@@ -27,6 +27,10 @@
 
 #include "msktutil.h"
 
+#if !defined(NS_MAXMSG)
+#define NS_MAXMSG 65535
+#endif
+
 std::string complete_hostname(const std::string &hostname)
 {
     // Ask the kerberos lib to canonicalize the hostname, and then pull it out of the principal.
