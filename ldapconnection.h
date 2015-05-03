a932af01e13dc511aca94386c0fff3f4db79fb6f
Mark Pröhl <mark@mproehl.net>
2015-05-03 11:50:22 +0200

    update copyright header

--- ldapconnection.h
+++ ldapconnection.h
@@ -1,3 +1,33 @@
+/*
+ *----------------------------------------------------------------------------
+ *
+ * ldapconnection.h
+ *
+ * (C) 2004-2006 Dan Perry (dperry@pppl.gov)
+ * (C) 2006 Brian Elliott Finley (finley@anl.gov)
+ * (C) 2009-2010 Doug Engert (deengert@anl.gov)
+ * (C) 2010 James Y Knight (foom@fuhm.net)
+ * (C) 2010-2013 Ken Dreyer <ktdreyer at ktdreyer.com>
+ * (C) 2012-2015 Mark Proehl <mark at mproehl.net>
+ * (C) 2012-2015 Olaf Flebbe <of at oflebbe.de>
+ *
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *-----------------------------------------------------------------------------
+ */
+
 #ifndef LDAPCONNECTION_H
 #define LDAPCONNECTION_H 1
 
