a48e08b6e9b1ff50106eba7e7073237c025bb28a
Daniel Kobras <kobras@puzzle-itc.de>
2021-09-23 16:28:26 +0200

    samba-password-command: fix shellcheck errors

--- samba-password-command.sh
+++ samba-password-command.sh
@@ -36,51 +36,44 @@ TDBTOOL="/usr/bin/tdbtool"
 NET="/usr/bin/net"
 TESTPARM="/usr/bin/testparm"
 
-samba_msg() {
-echo "$0: Error: "
-echo "       Adjust your SAMBA configuration first in /etc/samba/smb.conf"
-echo "       [global] section must contain at least following directives:"
-echo "       security  = ADS"
-echo "       workgroup = $WORKGROUP" 
+err_exit() {
+    echo "$0: Error in line $1: $ERR_MSG" >&2
 }
 
-[ "$UID" -ne 0 ] && echo "$0 must be run as root." && exit 1
+SAMBA_MSG="Invalid Samba configuration
+   Adjust your SAMBA configuration first in /etc/samba/smb.conf
+   [global] section must contain at least following directives:
+   security  = ADS
+   workgroup = $WORKGROUP"
 
-[ "x$WORKGROUP" == "x" ] && echo "$0: Error WORKGROUP not set" && exit 1
+trap 'err_exit $LINENO' EXIT
 
-[ "x$DCHOST" == "x" ] && echo "$0: Error DCHOST not set" && exit 1
+set -e
 
-if [ ! -x $TDBTOOL ] || [ ! -x $NET ]; then
-   echo "Error: tdbtool and net SAMBA utilities are not installed."
-   exit 1
-fi
+ERR_MSG="must be run as root."
+[ "$UID" -eq 0 ]
 
-$TESTPARM -l -s --parameter-name workgroup 2>/dev/null | /usr/bin/grep -q -x "$WORKGROUP"
+ERR_MSG="WORKGROUP not set"
+[ "x$WORKGROUP" != "x" ]
 
-[ $? -ne 0 ] && samba_msg && exit 1
+ERR_MSG="DCHOST not set"
+[ "x$DCHOST" != "x" ]
 
-$TESTPARM -l -s --parameter-name security 2>/dev/null | /usr/bin/grep -q -x "ADS"
+ERR_MSG="tdbtool and net SAMBA utilities are not installed."
+[ -x "$TDBTOOL" ] && [ -x "$NET" ]
 
-[ $? -ne 0 ] && samba_msg && exit 1
+ERR_MSG="$SAMBA_MSG"
+"$TESTPARM" -l -s --parameter-name workgroup 2>/dev/null | /usr/bin/grep -q -x "$WORKGROUP"
+"$TESTPARM" -l -s --parameter-name security 2>/dev/null | /usr/bin/grep -q -x "ADS"
 
-$NET -k -P rpc getsid -S $DCHOST
+ERR_MSG="$NET getsid failed"
+"$NET" -k -P rpc getsid -S "$DCHOST"
 
-[ $? -ne 0 ] && echo "$0: Error running $NET getsid" && exit 1
+ERR_MSG="$TDBTOOL failed"
+"$TDBTOOL" /var/lib/samba/private/secrets.tdb store SECRETS/MACHINE_LAST_CHANGE_TIME/"$WORKGROUP" 0000 2>/dev/null 1>/dev/null
+"$TDBTOOL" /var/lib/samba/private/secrets.tdb store SECRETS/MACHINE_PASSWORD.PREV/"$WORKGROUP" none 2>/dev/null 1>/dev/null
+"$TDBTOOL" /var/lib/samba/private/secrets.tdb store SECRETS/MACHINE_PASSWORD/"$WORKGROUP" none 2>/dev/null 1>/dev/null
 
-$TDBTOOL /var/lib/samba/private/secrets.tdb store SECRETS/MACHINE_LAST_CHANGE_TIME/"$WORKGROUP" 0000 2>/dev/null 1>/dev/null
-
-[ $? -ne 0 ] && echo "$0: Error running $TDBTOOL (1)" && exit 1
-
-$TDBTOOL /var/lib/samba/private/secrets.tdb store SECRETS/MACHINE_PASSWORD.PREV/"$WORKGROUP" none 2>/dev/null 1>/dev/null
-
-[ $? -ne 0 ] && echo "$0: Error running $TDBTOOL (2)" && exit 1
-
-$TDBTOOL /var/lib/samba/private/secrets.tdb store SECRETS/MACHINE_PASSWORD/"$WORKGROUP" none 2>/dev/null 1>/dev/null
-
-[ $? -ne 0 ] && echo "$0: Error running $TDBTOOL (3)" && exit 1
-
-exec $NET changesecretpw -f -i
-
-
-
+ERR_MSG="$NET changesecretpw failed"
+exec "$NET" changesecretpw -f -i
 
